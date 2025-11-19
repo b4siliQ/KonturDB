7a368e315b9126e44409b07901ff06f2ef2c88cb
b4siliQ <n.worakls@yandex.ru>
2025-11-19 23:35:24 +0700

    Feat: Настраивает динамическое расположение БД
    * Определяет путь к базе данных в папке `KonturSolutions` внутри `APPDATA`.
    * Использует `user.home` как запасной вариант, если `APPDATA` не доступен.
    * Автоматически создает папку `KonturSolutions`, если она не существует.
    * Инициализирует полный путь к файлу БД `KonturDB.db` статически.
    * Перенаправляет подключение `DriverManager` на новый динамический путь.

--- src/main/java/com/monolatte/kontur/model/SQLManager/SQLConnector.java
+++ src/main/java/com/monolatte/kontur/model/SQLManager/SQLConnector.java
@@ -1,15 +1,54 @@
 package com.monolatte.kontur.model.SQLManager;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class SQLConnector {
-    public static String database = "jdbc:sqlite:components.db";
-    public static Connection getConnection() {
-        Connection connect = null;
+    private static final String _FOLDERNAME = "KonturSolutions";
+    private static final String _FILENAME = "KonturDB.db";
+    private static final String _databasePath;
+
+    static {
         try {
-            connect = DriverManager.getConnection(database);
+            _databasePath = getDatabasePath();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String getDatabasePath() {
+        String appDataPath = System.getenv("APPDATA");
+        if (appDataPath.isEmpty()) {
+            appDataPath = System.getProperty("user.home");
+        }
+
+        File appDir = Paths.get(appDataPath, _FOLDERNAME).toFile();
+
+        if (!appDir.exists()) {
+            if (appDir.mkdirs()) {
+                System.out.printf(
+                        "Folder %s in appdata is online!%n\n",
+                        _FOLDERNAME
+                );
+            } else {
+                throw new RuntimeException(String.format(
+                        "Warning! Cannot init %s in appdata folder",
+                        _FOLDERNAME
+                ));
+            }
+        }
+
+        String fullPath = Paths.get(appDir.getAbsolutePath(), _FILENAME).toString();
+        return "jdbc:sqlite:" + fullPath;
+    }
+
+    public static Connection getConnection() {
+        Connection connect;
+        try {
+            connect = DriverManager.getConnection(_databasePath);
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage());
         }
