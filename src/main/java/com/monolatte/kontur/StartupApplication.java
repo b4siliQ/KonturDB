4bbbe2f039b93de72b77b6c275dab056c8a8ff61
b4siliQ <n.worakls@yandex.ru>
2025-11-25 12:05:22 +0700

    Feat: Инициализирует таблицы БД при запуске
    * Добавляет импорты для SQLSuperManager и ISQLManagerBase.
    * Вызывает метод _initTables() при старте приложения.
    * Реализует приватный метод _initTables() для создания всех необходимых таблиц БД.
    * Использует SQLSuperManager для получения всех зарегистрированных менеджеров и вызова createTable() для каждого.

--- src/main/java/com/monolatte/kontur/StartupApplication.java
+++ src/main/java/com/monolatte/kontur/StartupApplication.java
@@ -1,5 +1,7 @@
 package com.monolatte.kontur;
 
+import com.monolatte.kontur.model.SQLManager.ISQLManagerBase;
+import com.monolatte.kontur.model.SQLManager.SQLSuperManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,11 +12,18 @@ import java.io.IOException;
 public class StartupApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        this._initTables();
+
         FXMLLoader fxmlLoader = new FXMLLoader(StartupApplication.class.getResource("MainPanel.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
         stage.setTitle("Kontur");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void _initTables() {
+        SQLSuperManager.getInstance().getAllManagers()
+                .forEach(ISQLManagerBase::createTable);
     }
 
     public static void main(String[] args) {
