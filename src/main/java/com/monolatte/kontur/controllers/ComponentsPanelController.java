58a1a0904fb8d1149aac6da20757bd66ae03b6af
b4siliQ <n.worakls@yandex.ru>
2025-11-26 06:17:02 +0700

    Feat: Добавляет основные функции управления компонентами
    *   Реализована возможность удаления выбранного компонента из списка.
    *   Добавлен функционал открытия даташита компонента по указанной ссылке.
    *   Внедрена возможность добавления нового компонента, используя данные из полей ввода.
    *   Реализовано сохранение изменений для выбранного компонента.
    *   Добавлена обработка клика по элементу списка, отображающая его детали в полях ввода.
    *   Обновлены значения по умолчанию при добавлении пустого компонента.
    *   Добавлены необходимые импорты для работы с файлами и системным рабочим столом.
    *   Интегрирован обработчик `onMouseClicked` в `ComponentsPanel.fxml` для `ListView`.

--- src/main/java/com/monolatte/kontur/controllers/ComponentsPanelController.java
+++ src/main/java/com/monolatte/kontur/controllers/ComponentsPanelController.java
@@ -11,6 +11,11 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
 public class ComponentsPanelController {
     @FXML
     ListView<Component> compList;
@@ -47,33 +52,80 @@ public class ComponentsPanelController {
     @FXML
     public void _onAddEmptyButtonClicked() {
         this._manager.addNote(new Component(
-                "Empty",
-                "Controller",
-                "Specification here!",
-                "www.data.ru",
+                "Empty Name",
+                "Controller Type",
+                "Enter your specification here!",
+                "Enter your datasheet link here!",
                 100
-                ));
+        ));
         this._refreshList();
     }
 
     @FXML
     public void _onRemoveButtonClicked() {
-
+        var currentItem = this.compList.getSelectionModel().getSelectedItem();
+        if (currentItem == null) { return; }
+        this._manager.deleteNote(currentItem.getId());
+        this._refreshList();
     }
 
     @FXML
     public void _onOpenDatasheetButtonClicked() {
-
+        var currentItem = this.compList.getSelectionModel().getSelectedItem();
+        if (currentItem == null) { return; }
+        var pdfFile = new File(currentItem.getDatasheet_link());
+        try {
+            if (pdfFile.exists()) {
+                if (Desktop.isDesktopSupported()) {
+                    Desktop.getDesktop().open(pdfFile);
+                    System.out.println("Datasheet opened with supported default application\n");
+                } else {
+                    System.err.println("Datasheet can't be opened with current desktop environment\n");
+                }
+            } else {
+                System.err.printf("Current file on path '%s' cannot be opened. Try to check your file destination\n",
+                        currentItem.getDatasheet_link());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("An error occurred while trying to open the file");
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            System.err.println("File could not be opened (e.g., no application registered for PDFs).");
+        }
     }
 
     @FXML
     public void _onAddCompButtonClicked() {
+        this._manager.addNote(new Component(
+                this.nameCompTextField.getText(),
+                this.typeTextEdit.getText(),
+                this.compInfoTextArea.getText(),
+                this.datasheetPathTextField.getText(),
+                Integer.parseInt(this.costTextField.getText())
+        ));
+        this._refreshList();
+    }
 
+    // ! Исправить проблему с отказом сохранения данных
+    @FXML
+    public void _onSaveDataButtonClicked() {
+        var currentItem = this.compList.getSelectionModel().getSelectedItem();
+        if (currentItem == null) { return; }
+        this._manager.updateNote(currentItem);
+        this._refreshList();
     }
 
     @FXML
-    public void _onSaveDataButtonClicked() {
-
+    public void onCompListMouseClicked() {
+        var currentItem = this.compList.getSelectionModel().getSelectedItem();
+        if (currentItem == null) { return; }
+        this.idTextField.setText(String.valueOf(currentItem.getId()));
+        this.nameCompTextField.setText(currentItem.getName());
+        this.typeTextEdit.setText(currentItem.getType());
+        this.costTextField.setText(String.valueOf(currentItem.getPrice()));
+        this.compInfoTextArea.setText(currentItem.getSpecification());
+        this.datasheetPathTextField.setText(currentItem.getDatasheet_link());
     }
 
     private ObservableList<Component> _updateList() {
