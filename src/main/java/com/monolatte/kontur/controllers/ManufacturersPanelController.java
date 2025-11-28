2fdb51992268398a61aa6346f82a2d28a85c644f
b4siliQ <n.worakls@yandex.ru>
2025-11-28 14:04:51 +0700

    Feat: Инициализирует панель производителей
    *   Добавляет FXML поля в `ManufacturersPanelController` для элементов UI.
    *   Реализует FXML методы-обработчики событий в `ManufacturersPanelController`.
    *   Связывает элементы UI в `ManufacturersPanel.fxml` с контроллером через `fx:id` и обработчики событий.
    *   Импортирует модели `Component` и `Manufacturer` в контроллер.
    *   Устанавливает поле ID производителя как нередактируемое.
    *   Исправляет текст кнопки "Добавить нового пользователя" на "Добавить нового производителя".

--- src/main/java/com/monolatte/kontur/controllers/ManufacturersPanelController.java
+++ src/main/java/com/monolatte/kontur/controllers/ManufacturersPanelController.java
@@ -1,4 +1,60 @@
 package com.monolatte.kontur.controllers;
 
+import com.monolatte.kontur.model.Notes.Component;
+import com.monolatte.kontur.model.Notes.Manufacturer;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ManufacturersPanelController {
+    @FXML
+    ListView<Manufacturer> manufacturerListView;
+    @FXML
+    ListView<Component> componentsListView;
+    @FXML
+    Button addEmptyButton;
+    @FXML
+    Button removeButton;
+    @FXML
+    Button addManufacturerButton;
+    @FXML
+    Button saveDataButton;
+    @FXML
+    TextField idManufacturerTextField;
+    @FXML
+    TextField nameManufacturerTextField;
+    @FXML
+    TextArea descriptionManufacturerTextArea;
+
+    @FXML
+    public void onAddEmptyButtonClicked() {
+
+    }
+
+    @FXML
+    public void onRemoveButtonClicked() {
+
+    }
+
+    @FXML
+    public void onAddManufacturerButtonClicked() {
+
+    }
+
+    @FXML
+    public void onSaveDataButton() {
+
+    }
+
+    @FXML
+    public void onManufacturerListViewMouseClicked() {
+
+    }
+
+    @FXML
+    public void onComponentsListViewMouseClicked() {
+
+    }
 }
