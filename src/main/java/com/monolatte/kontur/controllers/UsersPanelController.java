b9772722eaddc69595054952600e7f268040f39c
b4siliQ <n.worakls@yandex.ru>
2025-11-28 12:06:41 +0700

    Feat: Настраивает контроллер и FXML панели пользователей
    *   Добавлены FXML-поля для `ListView` (списки пользователей и проектов), `Button` (добавить, удалить, добавить пользователя, сохранить данные), `TextField` (ID, имя пользователя) и `TextArea` (описание пользователя) в `UsersPanelController`.
    *   Добавлены FXML-методы-обработчики событий для интерактивных элементов в `UsersPanelController`.
    *   Добавлены необходимые импорты JavaFX для работы с FXML-элементами в `UsersPanelController`.
    *   Присвоены `fx:id` всем соответствующим элементам управления (`ListView`, `Button`, `TextField`, `TextArea`) в `UsersPanel.fxml`.
    *   Привязаны методы-обработчики к событиям `onAction` для кнопок и `onMouseClicked` для `ListView` в `UsersPanel.fxml`.

--- src/main/java/com/monolatte/kontur/controllers/UsersPanelController.java
+++ src/main/java/com/monolatte/kontur/controllers/UsersPanelController.java
@@ -1,4 +1,55 @@
 package com.monolatte.kontur.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class UsersPanelController {
+    @FXML
+    ListView<String> usersListView;
+    @FXML
+    Button addEmptyButton;
+    @FXML
+    Button removeButton;
+    @FXML
+    Button addUserButton;
+    @FXML
+    Button saveDataButton;
+    @FXML
+    TextField idUserTextField;
+    @FXML
+    TextField nameUserTextField;
+    @FXML
+    TextField descriptionUserTextField;
+
+    @FXML
+    public void onAddEmptyButtonClicked() {
+
+    }
+
+    @FXML
+    public void onRemoveButtonClicked() {
+
+    }
+
+    @FXML
+    public void onAddUserButtonClicked() {
+
+    }
+
+    @FXML
+    public void onSaveDataButtonClicked() {
+
+    }
+
+    @FXML
+    public void onUsersListViewMouseClicked() {
+
+    }
+
+    @FXML
+    public void onProjectsListViewMouseClicked() {
+
+    }
 }
