d59be5acbd643086d450b4a822f687d92e8dddce
b4siliQ <n.worakls@yandex.ru>
2025-11-29 09:17:34 +0700

    Feat: Добавляет привязку компонентов к производителям
    *   Добавляет кнопки "Привязать" и "Отвязать" для управления компонентами.
    *   Реализует функционал привязки компонентов к выбранному производителю через всплывающее окно поиска.
    *   Обновляет список компонентов, связанных с производителем, при его выборе.
    *   Рефакторинг методов обновления списков (`_refreshList` и `_updateList`) в `_refreshMainList` и `_updateMainList` для разделения логики.
    *   Добавляет новые методы `_updateComponentList` и `_refreshComponentLists` для управления списком привязанных компонентов.
    *   Добавляет необходимые импорты для работы с всплывающими окнами и DAO.
    *   Обновляет FXML-файл `ManufacturersPanel.fxml` для включения новых кнопок и изменения макета списка компонентов.

--- src/main/java/com/monolatte/kontur/controllers/ManufacturersPanelController.java
+++ src/main/java/com/monolatte/kontur/controllers/ManufacturersPanelController.java
@@ -3,17 +3,24 @@ package com.monolatte.kontur.controllers;
 import com.monolatte.kontur.model.Notes.Component;
 import com.monolatte.kontur.model.Notes.Manufacturer;
 import com.monolatte.kontur.model.Notes.Manufacturer_Usage;
-import com.monolatte.kontur.model.Notes.User;
+import com.monolatte.kontur.model.SQL.DAOFactory;
 import com.monolatte.kontur.model.SQL.ManufacturerDAO;
 import com.monolatte.kontur.model.SQL.Manufacturer_usageDAO;
 import com.monolatte.kontur.model.SQL.SQLTableManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ManufacturersPanelController {
     @FXML
@@ -29,6 +36,10 @@ public class ManufacturersPanelController {
     @FXML
     Button saveDataButton;
     @FXML
+    Button pinButton;
+    @FXML
+    Button unpinButton;
+    @FXML
     TextField idManufacturerTextField;
     @FXML
     TextField nameManufacturerTextField;
@@ -40,13 +51,13 @@ public class ManufacturersPanelController {
 
     @FXML
     public void initialize() {
-        this._refreshList();
+        this._refreshMainList();
     }
 
     @FXML
     public void onAddEmptyButtonClicked() {
         this._manufacturerDAO.addNote(new Manufacturer("new manufacture", "new manufacture"));
-        this._refreshList();
+        this._refreshMainList();
     }
 
     @FXML
@@ -54,7 +65,7 @@ public class ManufacturersPanelController {
         var currentManufacturer = manufacturerListView.getSelectionModel().getSelectedItem();
         if (currentManufacturer == null) { return; }
         this._manufacturerDAO.deleteNote(currentManufacturer.getId());
-        this._refreshList();
+        this._refreshMainList();
     }
 
     @FXML
@@ -63,7 +74,7 @@ public class ManufacturersPanelController {
                 this.nameManufacturerTextField.getText(),
                 this.descriptionManufacturerTextArea.getText()
         ));
-        this._refreshList();
+        this._refreshMainList();
     }
 
     @FXML
@@ -75,7 +86,51 @@ public class ManufacturersPanelController {
         currentManufacturer.setDescription(descriptionManufacturerTextArea.getText());
 
         this._manufacturerDAO.updateNote(currentManufacturer);
-        this._refreshList();
+        this._refreshMainList();
+    }
+
+    @FXML
+    public void onPinButtonClicked() {
+        var currentManufacturer = this.manufacturerListView.getSelectionModel().getSelectedItem();
+        try {
+            FXMLLoader popupLoader = new FXMLLoader(ManufacturersPanelController.class.getResource(
+                    "/com/monolatte/kontur/SearchPopup.fxml"
+            ));
+            Parent root = popupLoader.load();
+            SearchPopupController<Component> popupController = popupLoader.getController();
+
+            popupController.initDAO(DAOFactory.DAOType.COMPONENT);
+            popupController.initData();
+
+            Stage newStage = new Stage();
+            Scene newScene = new Scene(root);
+            newStage.setScene(newScene);
+            popupController.setStage(newStage);
+
+            newStage.setTitle("Component Searcher");
+            newStage.setResizable(false);
+            newStage.initModality(Modality.APPLICATION_MODAL);
+            newStage.showAndWait();
+
+            Component result = popupController.getChosenObject();
+            if (result != null) {
+                this._manufacturerUsageDAO.addNote(new Manufacturer_Usage(
+                        result.getId(),
+                        currentManufacturer.getId()
+                ));
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        this._refreshComponentLists();
+    }
+
+    @FXML
+    public void onUnpinButtonClicked() {
+        var currentManufacturer = manufacturerListView.getSelectionModel().getSelectedItem();
+
     }
 
     @FXML
@@ -86,6 +141,7 @@ public class ManufacturersPanelController {
         this.idManufacturerTextField.setText(String.valueOf(currentManufacturer.getId()));
         this.nameManufacturerTextField.setText(currentManufacturer.getName());
         this.descriptionManufacturerTextArea.setText(currentManufacturer.getDescription());
+        this._refreshComponentLists();
     }
 
     @FXML
@@ -93,12 +149,24 @@ public class ManufacturersPanelController {
 
     }
 
-    private ObservableList<Manufacturer> _updateList() {
+    private ObservableList<Manufacturer> _updateMainList() {
         return FXCollections.observableList(this._manufacturerDAO.getAllNotes());
     }
 
-    private void _refreshList() {
-        var update = this._updateList();
+    private void _refreshMainList() {
+        var update = this._updateMainList();
         this.manufacturerListView.setItems(update);
+    }
+
+    private ObservableList<Component> _updateComponentList() {
+        var currentManufacturer = this.manufacturerListView.getSelectionModel().getSelectedItem();
+        return FXCollections.observableList(this._manufacturerUsageDAO.getComponentsByManufacturerId(
+                currentManufacturer.getId()
+        ));
+    }
+
+    private void _refreshComponentLists() {
+        var update = this._updateComponentList();
+        this.componentsListView.setItems(update);
     }
 }
