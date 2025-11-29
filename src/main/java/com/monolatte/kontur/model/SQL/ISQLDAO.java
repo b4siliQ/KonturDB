cf3d61db947c79907cb8104c30b2ce095f18e75c
b4siliQ <n.worakls@yandex.ru>
2025-11-29 09:14:08 +0700

    Refactor: Ограничивает тип T в интерфейсах SQL DAO
    * Добавляет ограничение типа `<T extends BaseNote>` к интерфейсу `ISQLDAO`.
    * Добавляет ограничение типа `<T extends BaseNote>` к интерфейсу `ISQLDAOSearchable`.
    * Включает необходимый импорт `com.monolatte.kontur.model.Notes.BaseNote` для обоих интерфейсов.

--- src/main/java/com/monolatte/kontur/model/SQL/ISQLDAO.java
+++ src/main/java/com/monolatte/kontur/model/SQL/ISQLDAO.java
@@ -1,8 +1,10 @@
 package com.monolatte.kontur.model.SQL;
 
+import com.monolatte.kontur.model.Notes.BaseNote;
+
 import java.util.List;
 
-public interface ISQLDAO<T> extends ISQLDAOBase {
+public interface ISQLDAO<T extends BaseNote> extends ISQLDAOBase {
 
     void addNote(T note);
 
--- src/main/java/com/monolatte/kontur/model/SQL/ISQLDAOSearchable.java
+++ src/main/java/com/monolatte/kontur/model/SQL/ISQLDAOSearchable.java
@@ -1,7 +1,9 @@
 package com.monolatte.kontur.model.SQL;
 
+import com.monolatte.kontur.model.Notes.BaseNote;
+
 import java.util.List;
 
-public interface ISQLDAOSearchable<T> extends ISQLDAO<T> {
+public interface ISQLDAOSearchable<T extends BaseNote> extends ISQLDAO<T> {
     List<T> search(int searchType, String searchTerm);
 }
