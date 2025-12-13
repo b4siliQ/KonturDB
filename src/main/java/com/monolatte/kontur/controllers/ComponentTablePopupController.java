f3611f4f09455735d782948d655c4f8e6b16b28b
b4siliQ <n.worakls@yandex.ru>
2025-12-13 12:34:00 +0700

    Refactor: Удаляет неиспользуемый импорт
    * Удален неиспользуемый импорт `javafx.beans.property.SimpleStringProperty`

--- src/main/java/com/monolatte/kontur/controllers/ComponentTablePopupController.java
+++ src/main/java/com/monolatte/kontur/controllers/ComponentTablePopupController.java
@@ -5,7 +5,6 @@ import com.monolatte.kontur.model.Notes.Properties.ComponentProperty;
 import com.monolatte.kontur.model.SQL.ComponentsDAO;
 import com.monolatte.kontur.model.SQL.SQLTableManager;
 import javafx.beans.property.SimpleFloatProperty;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
