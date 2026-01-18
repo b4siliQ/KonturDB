6b064a85c3ed52694b62a0913f4b045f7c644df1
b4siliQ <n.worakls@yandex.ru>
2026-01-19 06:20:49 +0700

    Feat: Добавляет стилизацию UI и обновляет главный FXML
    *   Рефакторинг: Переместил классы `ISQLDAOBase` и `SQLTableManager` в пакет `service.SQL.Interfaces`.
    *   Документация: Добавил Javadoc комментарии для класса `StartupApplication` и методов `start`, `_initTables`.
    *   Обновление UI: Изменил основной FXML файл с `MainPanel.fxml` на `ComponentsPanel.fxml`.
    *   Новая функциональность: Внедрил динамическую загрузку и применение CSS-стилей (`Mineral Blue.css`).
    *   Рефакторинг UI: Инициализация сцены теперь использует `Parent` для корневого узла.
    *   Улучшение UI: Удалил жестко заданные размеры сцены, позволяя FXML определять их.

--- src/main/java/com/monolatte/kontur/StartupApplication.java
+++ src/main/java/com/monolatte/kontur/StartupApplication.java
@@ -1,26 +1,48 @@
 package com.monolatte.kontur;
 
-import com.monolatte.kontur.model.SQL.ISQLDAOBase;
-import com.monolatte.kontur.model.SQL.SQLTableManager;
+import com.monolatte.kontur.service.SQL.Interfaces.ISQLDAOBase;
+import com.monolatte.kontur.service.SQL.SQLTableManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Инициализирующий класс. Служит для запуска приложения.
+ *
+ * @author b4siliQ (Basil Makes Games)
+ * */
 public class StartupApplication extends Application {
+    /**
+     * Метод, инициализирующий стартовое окно и задающий параметры для запуска.
+     * */
     @Override
     public void start(Stage stage) throws IOException {
         this._initTables();
 
-        FXMLLoader fxmlLoader = new FXMLLoader(StartupApplication.class.getResource("MainPanel.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 810);
+        String cssPath = StartupApplication.class.getResource("/com/monolatte/kontur/CSS/Mineral Blue.css")
+                .toExternalForm();
+
+        var fxmlLoader = new FXMLLoader(StartupApplication.class.getResource(
+                "/com/monolatte/kontur/FXML/ComponentsPanel.fxml"
+        ));
+
+        Parent root = fxmlLoader.load();
+
+        var scene = new Scene(root);
+        root.getStylesheets().add(cssPath);
         stage.setTitle("KonturDB");
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * Метод, инициализирующий DAO компоненты для работы с таблицами в базе данных.
+     * Используется только при запуске приложения.
+     * */
     private void _initTables() {
         SQLTableManager.getInstance().getAllManagers()
                 .forEach(ISQLDAOBase::createTable);
